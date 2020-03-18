55d94d7e77012c78392f233d7567e1e788e634b6
Sebastian Nuss <sebastian.nuss@me.com>
2020-03-18 10:50:05 +0100

    Added Column

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,20 @@ import 'package:flutter/material.dart';
 var now = new DateTime.now();
 
 void main() => runApp(
-
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
             title: Text("Daytime"),
             backgroundColor: Colors.amber,
           ),
-          body: Center(
-            child: Image(
-              image: NetworkImage(
-                  "https://assets-ouch.icons8.com/thumb/464/83a0f14b-bcf8-4446-9c7a-228a67003a48.png"),
-            ),
-            child: Text("Hi");
+          body: Column(
+            children: <Widget>[
+              Image(
+                image: NetworkImage(
+                    "https://assets-ouch.icons8.com/thumb/464/83a0f14b-bcf8-4446-9c7a-228a67003a48.png"),
+              ),
+              Text("Hi"),
+            ],
           ),
         ),
       ),
