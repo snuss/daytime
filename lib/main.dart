75bbbaf342d5b966b68c91bd609c13052eec0dc2
Sebastian Nuss <sebastian.nuss@me.com>
2020-03-18 09:17:13 +0100

    Trying to add Text for date

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+var now = new DateTime.now();
+
 void main() => runApp(
+
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
@@ -12,6 +15,7 @@ void main() => runApp(
               image: NetworkImage(
                   "https://assets-ouch.icons8.com/thumb/464/83a0f14b-bcf8-4446-9c7a-228a67003a48.png"),
             ),
+            child: Text("Hi");
           ),
         ),
       ),
